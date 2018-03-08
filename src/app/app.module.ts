e083b7e3a4c2e4df0a1a04991d9875a2953f0936
Franz08 <blicanel@yahoo.com>
2018-03-09 02:27:59 +0800

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2'; //added
+import { AngularFirestoreModule } from 'angularfire2/firestore'; //added
+
+import { FormsModule } from '@angular/forms';
+
+var firebaseConfig = {
+	apiKey: "AIzaSyDINrv7WVK0-IVSjKj-Ndx5gbM4fU8Hn0o",
+    authDomain: "firestore-5534b.firebaseapp.com",
+    databaseURL: "https://firestore-5534b.firebaseio.com",
+    projectId: "firestore-5534b",
+    storageBucket: "firestore-5534b.appspot.com",
+    messagingSenderId: "740150838306"
+    } // added
 
 @NgModule({
   declarations: [
@@ -11,8 +24,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+    AngularFireModule.initializeApp(firebaseConfig), // added
+    AngularFirestoreModule, // added
+    FormsModule // added
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
